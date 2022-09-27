a2997f28ca80bc96c62d7762a2fda56481d7c1f6
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-09-27 15:01:15 -0400

    student controller update

--- CRS-Group-A-Java-Spring-REST/src/main/java/com/lti/restcontroller/StudentController.java
+++ CRS-Group-A-Java-Spring-REST/src/main/java/com/lti/restcontroller/StudentController.java
@@ -1,6 +1,9 @@
 package com.lti.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.service.StudentService;
 
 @RestController
 public class StudentController {
