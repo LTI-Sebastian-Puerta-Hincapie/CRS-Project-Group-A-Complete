d751b4d180ecfefbd283023d0778298dfcede155
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-09-27 17:31:46 -0400

    updated user controller get user method

--- CRS-Group-A-Java-Spring-REST/src/main/java/com/lti/restcontroller/StudentController.java
+++ CRS-Group-A-Java-Spring-REST/src/main/java/com/lti/restcontroller/StudentController.java
@@ -1,6 +1,11 @@
 package com.lti.restcontroller;
 
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.service.StudentService;
@@ -10,5 +15,16 @@ public class StudentController {
 
 	@Autowired 
 	private StudentService studentService;
+	
+	@RequestMapping(produces = MediaType.APPLICATION_JSON, 
+		    method = RequestMethod.POST,
+		    value = "/registerforcourse")
+		@ResponseBody
+			public void registerForCourse(){
+				
+				
+				
+				
+			}
 	
 }
