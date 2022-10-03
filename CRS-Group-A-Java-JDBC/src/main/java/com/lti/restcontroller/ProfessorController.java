2851abe0f3dfa716506763739857629d07451f90
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-03 14:16:46 -0400

    professor controller, service, dao + exception updates

--- CRS-Group-A-Java-JDBC/src/main/java/com/lti/restcontroller/ProfessorController.java
+++ CRS-Group-A-Java-JDBC/src/main/java/com/lti/restcontroller/ProfessorController.java
@@ -14,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lti.dto.Course;
 import com.lti.dto.CourseCatalog;
 import com.lti.dto.CourseEnrollment;
 import com.lti.dto.Grade;
 import com.lti.dto.Professor;
-import com.lti.dto.Student;
+import com.lti.exception.NoEnrolledStudentsFoundException;
+import com.lti.exception.ProfessorNotFoundException;
+import com.lti.exception.ProfessorNotRegisteredForCourseException;
 import com.lti.exception.StudentCourseNotFoundException;
 import com.lti.exception.StudentCourseRegistrationNotFoundException;
 import com.lti.exception.StudentNotFoundException;
@@ -33,7 +34,7 @@ public class ProfessorController {
 
 	@RequestMapping(produces = MediaType.APPLICATION_JSON, method = RequestMethod.GET, value = "/professors/{professorId}")
 	@ResponseBody
-	public ResponseEntity<Professor> getUser(@PathVariable("professorId") String professorId) {
+	public ResponseEntity getProfessor(@PathVariable("professorId") String professorId) throws ProfessorNotFoundException {
 		
 		Professor professor = null;
 		
@@ -46,7 +47,7 @@ public class ProfessorController {
 	@RequestMapping(produces = MediaType.APPLICATION_JSON, method = RequestMethod.GET, 
 			value = "/enrolled/students/{courseId}")
 	@ResponseBody
-	public ResponseEntity<List<CourseEnrollment>> viewEnrolledStudents(@PathVariable("courseId") String courseId) {
+	public ResponseEntity viewEnrolledStudents(@PathVariable("courseId") String courseId) throws NoEnrolledStudentsFoundException{
 
 		List<CourseEnrollment> courseEnrollmentList = professorServiceOperation
 				.viewEnrolledStudents(Integer.parseInt(courseId));
@@ -57,7 +58,7 @@ public class ProfessorController {
 	@RequestMapping(produces = MediaType.APPLICATION_JSON, method = RequestMethod.GET, 
 			value = "/courses/professor/{professorId}")
 	@ResponseBody
-	public ResponseEntity<List<CourseCatalog>> getProfessorCourses(@PathVariable("professorId") String professorId) {
+	public ResponseEntity getProfessorCourses(@PathVariable("professorId") String professorId) throws ProfessorNotRegisteredForCourseException {
 
 		List<CourseCatalog> courseEnrollmentList = professorServiceOperation
 				.getProfessorCourses(Integer.parseInt(professorId));
@@ -68,18 +69,14 @@ public class ProfessorController {
 	@RequestMapping(produces = MediaType.APPLICATION_JSON, method = RequestMethod.POST, 
 			value = "/add/grades/{studentId}")
 	@ResponseBody
-	public ResponseEntity<Boolean> addGrades(@PathVariable("studentId") String studentId, 
-			@RequestBody Grade grade) {
+	public ResponseEntity addGrades(@PathVariable("studentId") String studentId, 
+			@RequestBody Grade grade) throws NumberFormatException, StudentNotFoundException,
+	StudentCourseNotFoundException, StudentCourseRegistrationNotFoundException {
 
-		// TODO exception handling
-		try {
 			professorServiceOperation.addGrades(Integer.parseInt(studentId),
 					 grade.getCourse().getCourseId(), grade.getGrade());
-		} catch (NumberFormatException | StudentNotFoundException | StudentCourseNotFoundException
-				| StudentCourseRegistrationNotFoundException e) {
-			return new ResponseEntity(Boolean.FALSE, HttpStatus.OK);
-		}
-
-		return new ResponseEntity(Boolean.TRUE, HttpStatus.OK);
+		
+		return new ResponseEntity("Grade has been added successfully", HttpStatus.OK);
 	}
+	
 }
