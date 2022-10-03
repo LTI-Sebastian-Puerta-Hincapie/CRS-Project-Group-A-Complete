97226b9117a6e292ef850037e2a5f38be289b081
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-03 14:21:28 -0400

    exception updates

--- CRS-Group-A-Java-JDBC/src/main/java/com/lti/exception/StudentCourseNotFoundException.java
+++ CRS-Group-A-Java-JDBC/src/main/java/com/lti/exception/StudentCourseNotFoundException.java
@@ -7,18 +7,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class StudentCourseNotFoundException extends Exception {
 	
 	public StudentCourseNotFoundException() {
+		super("\nStudent course not found");
 		System.out.println("\nStudent course not found");
 	}
 	
 	public StudentCourseNotFoundException(String message) {
+		super(message);
 		System.out.println(message);
 	}
 	
 	public StudentCourseNotFoundException(Throwable cause) {
+		super("\nStudent course not found");
 		System.out.println(cause);
 	}
 	
 	public StudentCourseNotFoundException(String message, Throwable cause) {
+		super(message, cause);
 		System.out.println(message);
 		System.out.println(cause);
 	}
--- CRS-Group-A-Java-JDBC/src/main/java/com/lti/exception/StudentCourseRegistrationNotFoundException.java
+++ CRS-Group-A-Java-JDBC/src/main/java/com/lti/exception/StudentCourseRegistrationNotFoundException.java
@@ -1,12 +1,18 @@
 package com.lti.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class StudentCourseRegistrationNotFoundException extends Exception {
 	
 	public StudentCourseRegistrationNotFoundException() {
+		super("\nStudent has not been registered");
 		System.out.println("\nStudent has not been registered");
 	}
 	
 	public StudentCourseRegistrationNotFoundException(String e) {
+		super(e);
 		System.out.println(e);
 	}
 }
