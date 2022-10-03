d9c5c46b3251510234c4204eb54d119669e24a26
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-03 10:17:55 -0400

    other service updates

--- CRS-Group-A-Java-JDBC/src/main/java/com/lti/service/CourseCatalogService.java
+++ CRS-Group-A-Java-JDBC/src/main/java/com/lti/service/CourseCatalogService.java
@@ -2,6 +2,7 @@ package com.lti.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lti.dao.CourseCatalogDAO;
@@ -17,12 +18,8 @@ import com.lti.dto.CourseCatalog;
 @Service
 public class CourseCatalogService implements CourseCatalogOperation {
 	
+	@Autowired
 	private CourseCatalogDAO courseCatalogDao;
-	
-	public CourseCatalogService() {
-		
-		this.courseCatalogDao = new CourseCatalogDAOImpl();
-	}
 
 	@Override
 	public List<CourseCatalog> ListOfAllCourses() {
