dc262a4118c6b93e11d36ffde57176b916e3e0c7
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-04 10:13:34 -0400

    professor api updates

--- CRS-Group-A-Java-JDBC/src/main/java/com/lti/restcontroller/ProfessorController.java
+++ CRS-Group-A-Java-JDBC/src/main/java/com/lti/restcontroller/ProfessorController.java
@@ -44,7 +44,7 @@ public class ProfessorController {
 
 	@RequestMapping(produces = MediaType.APPLICATION_JSON,
 			method = RequestMethod.GET, 
-			value = "professor/enrolled/course/{courseId}")
+			value = "professor/enrolled/courses/{courseId}")
 	@ResponseBody
 	public ResponseEntity viewEnrolledStudents(@PathVariable("courseId") String courseId) throws NoEnrolledStudentsFoundException{
 
@@ -54,26 +54,31 @@ public class ProfessorController {
 		return new ResponseEntity<List<CourseEnrollment>>(courseEnrollmentList, HttpStatus.OK);
 	}
 
-	@RequestMapping(produces = MediaType.APPLICATION_JSON, method = RequestMethod.GET, 
-			value = "/courses/professor/{professorId}")
+	@RequestMapping(produces = MediaType.APPLICATION_JSON, 
+			method = RequestMethod.GET, 
+			value = "/professor/{professorId}/courses")
 	@ResponseBody
-	public ResponseEntity getProfessorCourses(@PathVariable("professorId") String professorId) throws ProfessorNotRegisteredForCourseException {
+	public ResponseEntity getProfessorCourses(@PathVariable("professorId") int professorId) throws ProfessorNotRegisteredForCourseException {
 
 		List<CourseCatalog> courseEnrollmentList = professorServiceOperation
-				.getProfessorCourses(Integer.parseInt(professorId));
+				.getProfessorCourses(professorId);
 
 		return new ResponseEntity<List<CourseCatalog>>(courseEnrollmentList, HttpStatus.OK);
 	}
 	
-	@RequestMapping(produces = MediaType.APPLICATION_JSON, method = RequestMethod.POST, 
-			value = "/add/grades/{studentId}")
+	@RequestMapping(produces = MediaType.APPLICATION_JSON, 
+			method = RequestMethod.POST, 
+			value = "/professor/addgrades/{studentId}")
 	@ResponseBody
-	public ResponseEntity addGrades(@PathVariable("studentId") String studentId, 
+	public ResponseEntity addGrades(
+			@PathVariable("studentId") int studentId, 
 			@RequestBody Grade grade) throws NumberFormatException, StudentNotFoundException,
 	StudentCourseNotFoundException, StudentCourseRegistrationNotFoundException {
 
-			professorServiceOperation.addGrades(Integer.parseInt(studentId),
-					 grade.getCourse().getCourseId(), grade.getGrade());
+			professorServiceOperation.addGrades(
+					studentId, 
+					grade.getCourse().getCourseId(), 
+					grade.getGrade());
 		
 		return new ResponseEntity("Grade has been added successfully", HttpStatus.OK);
 	}
