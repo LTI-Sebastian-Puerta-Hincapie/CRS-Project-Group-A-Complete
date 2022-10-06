45de4993a729324dcf22969908435773732c2d46
jonathanyeh98 <yehjonathan98@gmail.com>
2022-10-06 13:58:03 -0400

    Initial commit for admin producer

---
+++ CRS-Group-A-Admin-Producer/src/main/java/com/lti/exception/SemesterRegistrationNotApprovedException.java
@@ -0,0 +1,27 @@
+package com.lti.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.lti.dto.User;
+
+@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+public class SemesterRegistrationNotApprovedException extends Exception {
+	
+	public SemesterRegistrationNotApprovedException(User user) {
+		System.out.println("\nThe registration for " + user.getUsername() + " has not been approved");
+	}
+	
+	public SemesterRegistrationNotApprovedException(String message) {
+		System.out.println(message);
+	}
+	
+	public SemesterRegistrationNotApprovedException(Throwable cause) {
+		System.out.println(cause);
+	}
+	
+	public SemesterRegistrationNotApprovedException(String message, Throwable cause) {
+		System.out.println(message);
+		System.out.println(cause);
+	}
+}
---
+++ CRS-Group-A-Admin-Producer/src/main/java/com/lti/service/AdminServiceOperation.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package com.lti.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lti.dto.Admin;
+import com.lti.dto.Course;
+import com.lti.dto.Grade;
+import com.lti.dto.Professor;
+import com.lti.dto.SemesterRegistration;
+import com.lti.dto.Student;
+import com.lti.exception.CourseNotFoundException;
+import com.lti.exception.SemesterRegistrationExistsException;
+import com.lti.exception.UserNotFoundException;
+
+/**
+ * @author Jonathan
+ *
+ */
+public interface AdminServiceOperation {
+	
+	/**
+	 * This method generates a report card for the student
+	 * @param studentID of type integer
+	 */
+	public List<Grade> generateReportCard(int studentID);
+	
+	/**
+	 * This method adds a professor to the database
+	 * @param professor of type Professor
+	 */
+	public void addProfessor(Professor professor);
+	
+	/**
+	 * This method approves the registration of a student's course
+	 * @param studentID of type integer 
+	 * @param approvalStatus of type integer 
+	 */
+	public void approveStudentRegistration(int studentID, int approvalStatus);
+	
+	/**
+	 * Creates StudentRegistration in database
+	 * @param semesterRegistration of type SemesterRegistration
+	 */
+	public void createStudentRegistration(SemesterRegistration semesterRegistration) throws SemesterRegistrationExistsException, UserNotFoundException;
+	
+	/**
+	 * This method adds a course to the course list
+	 * @param course of type Course
+	 */
+	public void addCourse(Course course);
+	
+	/**
+	 * This method removes the course from the course list
+	 * @param courseId of type integer
+	 * @throws CourseNotFoundException 
+	 */
+	public void removeCourse(int courseId) throws CourseNotFoundException;
+	
+	/**
+	 * This method updates a course's information in the course list
+	 * @param courseId of type integer
+	 * @param courseName of type String
+	 * @param description of type String
+	 * @throws CourseNotFoundException 
+	 */
+	public void updateCourse(int courseId, String courseName, String description) throws CourseNotFoundException;
+	
+	/**
+	 * This method checks if a course is available for enrollment
+	 * @param courseId of type integer
+	 * @throws CourseNotFoundException 
+	 */
+	public Boolean checkAvailability(int courseId) throws CourseNotFoundException;
+	
+	/**
+	 * This method checks the list of courses a student is registered to
+	 * @param studentID of type integer
+	 * @return 
+	 */
+	public List<Course> viewCourses(int studentID);
+	
+	/**
+	 * This method checks the list of courses a student is registered to
+	 * @param studentId of type integer
+	 * @return SemesterRegistration returns a list of registered students for the semester
+	 */
+	public SemesterRegistration getSemesterRegistration(int studentId);
+	
+	/**
+	 * This method updates the enrollment count in the course catalog for a specific course
+	 * @param courseId of type integer
+	 * @return SemesterRegistration returns a list of registered students for the semester
+	 */
+	public void updateCourseCatalogEnrollmentForCourse(int courseId);
+}
