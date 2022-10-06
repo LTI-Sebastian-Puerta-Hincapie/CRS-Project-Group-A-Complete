42aced4daa3e0fb55a62b8427c7274e878d0abdd
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-06 11:16:04 -0400

    saving changes

---
+++ CRS-Group-A-Student-Producer/src/main/java/com/lti/application/CrsGroupAStudentProducerApplication.java
@@ -0,0 +1,13 @@
+package com.lti.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsGroupAStudentProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsGroupAStudentProducerApplication.class, args);
+	}
+
+}
