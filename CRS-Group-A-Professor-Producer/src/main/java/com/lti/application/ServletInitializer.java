32e87509ba6aa444bbc142bcbb5f02be0518b275
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-07 09:43:23 -0400

    bug updates

--- CRS-Group-A-Professor-Producer/src/main/java/com/lti/application/ServletInitializer.java
+++ CRS-Group-A-Professor-Producer/src/main/java/com/lti/application/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.lti.CRSGroupAProfessorProducer;
+package com.lti.application;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
