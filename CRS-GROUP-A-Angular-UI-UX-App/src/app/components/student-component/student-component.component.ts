f0f51160fd14fc7b4db2eab68733a2a3fba61415
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-14 11:18:07 -0400

    added initial methods to the student component

--- CRS-GROUP-A-Angular-UI-UX-App/src/app/components/student-component/student-component.component.ts
+++ CRS-GROUP-A-Angular-UI-UX-App/src/app/components/student-component/student-component.component.ts
@@ -12,4 +12,31 @@ export class StudentComponentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addCourse() {
+    console.log("add course method");
+  }
+
+  dropCourse() {
+    console.log("drop course method");
+  }
+
+  registerCourse() {
+    console.log("register course method");
+  }
+
+  makePayment() {
+    console.log("make payment method");
+  }
+
+  viewStudent() {
+    console.log("view student method");
+  }
+
+  viewAddedCourses() {
+    console.log("view added courses method");
+  }
+
+  viewRegisteredCourses() {
+    console.log("view registered courses method");
+  }
 }
