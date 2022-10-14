4591b79e0ff111dd30c426751d80ba09d2d5e534
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-14 14:43:29 -0400

    updated course component

--- CRS-GROUP-A-Angular-UI-UX-App/src/app/components/course-component/course-component.component.ts
+++ CRS-GROUP-A-Angular-UI-UX-App/src/app/components/course-component/course-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course';
 
 @Component({
   selector: 'app-course-component',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponentComponent implements OnInit {
 
+  courses:Array<Course> = [
+    new Course(1, "Biology 1", "Biology"),
+    new Course(2, "Biology 2", "Biology"),
+    new Course(3, "Biology 3", "Biology")
+  ];
+
+  studentCourses:Array<Course> = new Array();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addCourse(courseId: number) {
+    console.log("add course method");
+
+    // pre-condition
+    if(this.studentCourses.find(x => x.getCourseId() == courseId) != undefined) return;
+
+    this.studentCourses.push(this.courses.filter(x => x.getCourseId() == courseId)[0]);
+  }
+
+  dropCourse(courseId: number) {
+    console.log("drop course method");
+    
+    // pre-condition
+    if(this.studentCourses.find(x => x.getCourseId() == courseId) == undefined) return;
+
+    let element = this.courses.filter(x => x.getCourseId() == courseId)[0];
+    let index = this.courses.indexOf(element, 0);
+    this.studentCourses.splice(index, 1);
+    // delete this.studentCourses[index];
+    
+    console.log(this.studentCourses);
+  }
+
+  registerCourse() {
+    console.log("register course method");
+  }
+
+  makePayment() {
+    console.log("make payment method");
+  }
+
+  viewStudent() {
+    console.log("view student method");
+  }
+
+  viewAddedCourses() {
+    console.log("view added courses method");
+  }
+
+  viewRegisteredCourses() {
+    console.log("view registered courses method");
+  }
 }
