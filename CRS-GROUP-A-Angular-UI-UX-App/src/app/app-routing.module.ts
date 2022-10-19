875315256377a65ef0e3bf525c0a40e29ce105a4
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-19 12:17:34 -0400

    added json files, json-server, and routing to ui-ux project

--- CRS-GROUP-A-Angular-UI-UX-App/src/app/app-routing.module.ts
+++ CRS-GROUP-A-Angular-UI-UX-App/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseComponentComponent } from './components/course-component/course-component.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { PaymentComponentComponent } from './components/payment-component/payment-component.component';
+import { SemesterRegistrationComponentComponent } from './components/semester-registration-component/semester-registration-component.component';
+import { StudentViewComponentComponent } from './components/student-view-component/student-view-component.component';
+import { UpdatePasswordComponentComponent } from './components/update-password-component/update-password-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponentComponent},
+  {path:'updatepassword', component:UpdatePasswordComponentComponent},
+  {path:'semesterregistration', component:SemesterRegistrationComponentComponent},
+  {path:'courses', component: CourseComponentComponent},
+  {path:'payment', component: PaymentComponentComponent},
+  {path:'studentview', component: StudentViewComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
