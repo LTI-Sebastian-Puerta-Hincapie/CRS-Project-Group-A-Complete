31209dafe24ec87ec446a3469b6f5adc566c52fa
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-10-21 14:12:12 -0400

    student component views added

--- CRS-GROUP-A-Angular-UI-UX-App/src/app/services/student-service.service.ts
+++ CRS-GROUP-A-Angular-UI-UX-App/src/app/services/student-service.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentServiceService {
 
-  constructor() { }
+  // cros implementation
+  headers = new HttpHeaders()
+  .set('Content-Type', 'application/json')
+  .set('Access-Control-Allow-Origin',"*");
+
+  constructor(private httpClient:HttpClient) { }
+
+  // SERVICE METHODS
+  //GET
+  getStudent(studentId:number):Observable<any> {
+    console.log("Get student method");
+    let uri:string = "http://localhost:8092/student/" + studentId; 
+    return this.httpClient.get(uri, {headers:this.headers});
+  }
+
+  getStudentCourses(studentId:number):Observable<any> {
+    console.log("Get student method");
+    let uri:string = "http://localhost:8092/student/" + studentId + "/courses"; 
+    return this.httpClient.get(uri, {headers:this.headers});
+  }
+
+  getStudentRegisteredCourses(studentId:number):Observable<any> {
+    console.log("Get student method");
+    let uri:string = "http://localhost:8092/student/" + studentId + "/registeredcourses"; 
+    return this.httpClient.get(uri, {headers:this.headers});
+  }
+
+  getStudentGrades(studentId:number):Observable<any> {
+    console.log("Get student method");
+    let uri:string = "http://localhost:8092/student/" + studentId + "/viewgrades"; 
+    return this.httpClient.get(uri, {headers:this.headers});
+  }
+
+  //POST
+
+  //PUT
+
+  //DELETE
+
+
 }
