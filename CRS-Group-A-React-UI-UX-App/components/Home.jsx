bb6e785ca6f6ded2ec69f935b0737453fdd080be
LTI-Sebastian-Puerta-Hincapie <sebastian.puertahincapie@lntinfotech.com>
2022-11-08 11:39:47 -0500

    added user dashboard to the home screen

--- CRS-Group-A-React-UI-UX-App/components/Home.jsx
+++ CRS-Group-A-React-UI-UX-App/components/Home.jsx
@@ -4,18 +4,49 @@ import PremiumEmployee from "./PremiumEmployee.jsx";
 import {Link } from 'react-router';
 
 class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            showNormal: false,
+            showPremium: false
+        }
+    }
+
+ componentDidMount() {
+    this.state = {
+      showNormal: false,
+      showPremium: false
+    };
+  }
+
+  showNormal() {
+    this.setState({
+      showNormal: !this.state.showNormal
+    });
+  }
+
+  showPremium() {
+    this.setState({
+      showPremium: !this.state.showPremium
+    });
+  }
 
  render(){
 
     let link = {
         color: "black",
-        fontFamily: "Robot"
+        fontFamily: "Robot",
+        textDecoration: "none"
     }
 
     let button = {
         margin: 10,
         padding: "1em",
-        backgroundColor: "blue"
+        backgroundColor: "LightGreen",
+        borderRadius: "8px",
+        border: "none",
+        color: "black",
+        fontFamily: "Robot"
     }
     let container = {
         padding: "2em",
@@ -28,10 +59,13 @@ class Home extends React.Component{
       return (
           <div style={container}>
              <h1> Welcome ! </h1>
-             <button style={button}><Link to="normalemployee" style={link}>NormalEmployee</Link></button>
-             <button style={button}><Link to="premiumemployee" style={link}>PremiumEmployee</Link></button>
+             <button style={button}><Link to="normalemployee" style={link}>Navigate to Normal </Link></button>
+             <button style={button}><Link to="premiumemployee" style={link}>Navigate to Premium </Link></button><br/><br/>
+             <button style={button} onClick={()=>this.showNormal()}>Show Normal</button>
+             <button style={button} onClick={()=>this.showPremium()}>Show Premium</button>
+             {this.state.showNormal && <NormalEmployee/>}
+             {this.state.showPremium && <PremiumEmployee/>}
           </div>
-          
       );
  }
 
